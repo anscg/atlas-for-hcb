9fd3223373b1962574f69ae6e18ba22a0778a589
Anson Chung <58066418+anscg@users.noreply.github.com>
2025-04-08 13:21:05 +0800

    DEP*OY

--- vite.config.ts
+++ vite.config.ts
@@ -8,21 +8,35 @@ declare module "@remix-run/node" {
   }
 }
 
-export default defineConfig({
-  ssr: {
-    noExternal: ["@silk-hq/components"],
-  },
+// Pass a function to defineConfig
+export default defineConfig(({ command }) => {
+  // 'command' will be 'build' or 'serve'
+  const isProduction = command === "build";
 
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+  // Return your configuration object from the function
+  return {
+    ssr: {
+      noExternal: ["@silk-hq/components"],
+    },
+    plugins: [
+      remix({
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+          v3_singleFetch: true,
+          v3_lazyRouteDiscovery: true,
+        },
+      }),
+      tsconfigPaths(),
+    ],
+    resolve: isProduction // Use the 'isProduction' boolean derived from 'command'
+      ? {
+          alias: {
+            // Force 'react-dom/server' to resolve to the Node.js version
+            "react-dom/server": "react-dom/server.node.js",
+          },
+        }
+      : {}, // No alias needed for development server
+  };
 });
